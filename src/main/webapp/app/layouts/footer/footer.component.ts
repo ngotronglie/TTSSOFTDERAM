3400d22dcc785a5e2db8a134e83b3fd7ff4ae5bf
Liem <ngotrongliem2004@gmail.com>
2025-03-27 16:56:01 +0700

    hoan thien footer co du reponsive

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { TranslateDirective } from 'app/shared/language';
-
 @Component({
   selector: 'jhi-footer',
   templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
   imports: [TranslateDirective],
 })
 export default class FooterComponent {}
