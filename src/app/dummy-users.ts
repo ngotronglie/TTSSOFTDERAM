63bf1405cb703778d9b418df0b66715bab55a052
Liem <ngotrongliem2004@gmail.com>
2025-03-30 16:17:09 +0700

    add arvatar image

--- src/app/dummy-users.ts
+++ src/app/dummy-users.ts
@@ -2,31 +2,31 @@ export const DUMMY_USERS = [
   {
     id: 'u1',
     name: 'Jasmine Washington',
-    avatar: 'user-1.jpg',
+    avatar: 'user-1.png',
   },
   {
     id: 'u2',
     name: 'Emily Thompson',
-    avatar: 'user-2.jpg',
+    avatar: 'user-2.png',
   },
   {
     id: 'u3',
     name: 'Marcus Johnson',
-    avatar: 'user-3.jpg',
+    avatar: 'user-3.png',
   },
   {
     id: 'u4',
     name: 'David Miller',
-    avatar: 'user-4.jpg',
+    avatar: 'user-4.png',
   },
   {
     id: 'u5',
     name: 'Priya Patel',
-    avatar: 'user-5.jpg',
+    avatar: 'user-5.png',
   },
   {
     id: 'u6',
     name: 'Arjun Singh',
-    avatar: 'user-6.jpg',
+    avatar: 'user-6.png',
   },
 ];
