6988f85d75e33efeb40f13af2e04b84ff51ea2cb
Ngô Trọng Liêm <ngotrongliem2004@gmail.com>
2025-05-07 14:06:05 +0700

    config pom.xml security

---
+++ src/main/java/com/example/backend/security/JwtUtil.java
@@ -0,0 +1,38 @@
+package com.example.backend.security;
+
+import lombok.Value;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+//    @Value("${jwt.secret}")
+    private String secret;
+
+    private final long expiration = 1000 * 60 * 60 * 10; // 10h
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateToken(String token, String email) {
+        return email.equals(extractEmail(token)) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getExpiration().before(new Date());
+    }
+}
